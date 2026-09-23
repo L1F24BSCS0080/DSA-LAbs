32f9b476542cd119a73f551004feac65a4d23355
user <asadrana0342@gmail.com>
2026-09-23 23:29:36 +0500

    removed bst insertions

--- task1.cpp
+++ task1.cpp
@@ -4,10 +4,10 @@ int main() {
 	bst.insert(10);
 	bst.insert(5);
 	bst.insert(15);
-	bst.insert(1);
-	bst.insert(7);
-	bst.insert(12);
-	bst.insert(17);
+	// bst.insert(1);
+	// bst.insert(7);
+	// bst.insert(12);
+	// bst.insert(17);
 	bst.BFT();
 	cout << endl;
 
